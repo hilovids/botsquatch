cbaa01c28ba798f337801d3500ba6ea30aef0c1d
Davis Murphy <49877389+hilovids@users.noreply.github.com>
2024-03-16 17:48:11 -0400

    update scan

--- commands/camp-hilo/scscan.js
+++ commands/camp-hilo/scscan.js
@@ -115,7 +115,7 @@ module.exports = {
             return;
         }
 
-        // await updateInfo(connection, discordUser);
+        await updateInfo(connection, discordUser);
 
         const exampleEmbed = new EmbedBuilder()
         .setColor(0x003280)
